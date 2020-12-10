3eee6748e789e8e8c84e78aca549d60301760e68
Artemy Krupsky <4xel1337@gmail.com>
2020-12-10 04:13:04 +0300

    Added module test

---
+++ UnitTestLab_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestLab_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestLab_1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5c984ca2-8f30-4a1f-bbee-dda20b48757a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestLab_1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestLab_1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] arr = new int[] { 20, 5, 8, 2, 365, 108 };
+            int[] arrSorted = new int[] { 2, 5, 8, 20, 108, 365 };
+            Array.Sort(arr);
+            CollectionAssert.AreEqual(arrSorted, arr);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] arr = new int[] { 20, 5, 8, 2, 365, -108 };
+            int[] arrSorted = new int[] { -108, 2, 5, 8, 20, 365 };
+            Array.Sort(arr);
+            CollectionAssert.AreEqual(arrSorted, arr);
+        }
+
+    }
+}
